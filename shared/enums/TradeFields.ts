bedcf5ca8b2c0e79d821f68c1123cd73937ad278
kanera <kaner.assaf@gmail.com>
2025-07-11 15:29:04 +0300

    change labels order for order of display

--- shared/enums/TradeFields.ts
+++ shared/enums/TradeFields.ts
@@ -1,14 +1,14 @@
 import type { NewTradeFields, PositionExit } from "../types";
 
 export const AddTradeEntryLabels: Record<keyof Omit<NewTradeFields, "exits">, string> = {
+    ticker: "Stock Symbol",
+    entryPrice: "Entry Price",
     entryAmount: "Entry Amount",
     entryDate: "Entry Date",
-    entryPrice: "Entry Price",
-    ticker: "Stock Symbol",
 }
 
 export const AddTradeExitsLabels: Record<keyof PositionExit, string> = {
+    price: "Price",
     amount: "Amount",
     date: "Exit Date",
-    price: "Price"
 }
