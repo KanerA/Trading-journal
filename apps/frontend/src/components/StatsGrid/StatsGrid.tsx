c5c42ce1b224a2de7d96ac99e99a34f8c05ea4aa
kanera <kaner.assaf@gmail.com>
2025-07-12 14:30:24 +0300

    fix: format Total P&L value to two decimal places in StatsGrid component

--- apps/frontend/src/components/StatsGrid/StatsGrid.tsx
+++ apps/frontend/src/components/StatsGrid/StatsGrid.tsx
@@ -5,11 +5,12 @@ import StatCard from './StatCard/StatCard';
 
 export default function StatsGrid() {
     // TODO: maybe create enum with labels and conditions for these cards
+    // ?: maybe option to choose what currency is used
     const mainStats = useSelector(getTradesStats)
 
     return (
         <Box sx={{ width: "95vw", margin: "auto", display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gap: "1.5rem" }}>
-            <StatCard label="Total P&L" value={mainStats.totalPnL} isWin={mainStats.totalPnL >= 0} />
+            <StatCard label="Total P&L" value={`$${mainStats.totalPnL.toFixed(2)}`} isWin={mainStats.totalPnL >= 0} />
             <StatCard label="Win Rate" value={mainStats.winRate} />
             <StatCard label="Winning Trades" value={mainStats.winners} isWin={mainStats.totalPnL >= 0} />
             <StatCard label="Losing Trades" value={mainStats.losers} isWin={mainStats.totalPnL <= 0} />
