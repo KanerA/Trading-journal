f6cf3228561388e93369cad3c62877f79f9ea876
kanera <kaner.assaf@gmail.com>
2025-07-12 14:09:19 +0300

    refactor: simplify StatsGrid component by using Redux for mainStats and remove local state management

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,6 +1,4 @@
 import { Box } from '@mui/material';
-import type { Trade } from '@trading-journal/shared';
-import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import StatsGrid from '../components/StatsGrid/StatsGrid.tsx';
 import Tabs from '../components/Tabs/Tabs.tsx';
@@ -14,33 +12,15 @@ export interface MainStats {
 }
 
 const MainPage = () => {
-    const [mainStats, setMainStats] = useState<MainStats>({
-        totalPnL: 0,
-        winRate: "0%",
-        winners: 0,
-        losers: 0,
-    });
+
 
     const trades = useSelector(getAllTrades)
 
-    const calcStats = (trades: Trade[]) => {
-        return trades.reduce((prev, acc) => {
-            if (acc.outcome === "loser") prev["losers"] += 1;
-            if (acc.outcome === "winner") prev["winners"] += 1;
-            prev.totalPnL += acc.pnl
-            prev.winRate = `${prev.winners / (prev.winners + prev.losers) * 100}%`;
-            return prev;
-        }, { ...mainStats });
-    }
 
-    useEffect(() => {
-        const calculatedStats = calcStats(trades);
-        setMainStats(calculatedStats)
-    }, [])
 
     return (
         <Box>
-            <StatsGrid mainStats={mainStats} />
+            <StatsGrid />
             <Tabs trades={trades} />
         </Box>
     );
--- apps/frontend/src/store/selectors/tradeSelector.ts
+++ apps/frontend/src/store/selectors/tradeSelector.ts
@@ -1,3 +1,24 @@
+import { Outcome, type Trade } from "@trading-journal/shared";
 import type { RootState } from "../index.ts";
 
+const calcStats = (trades: Trade[]) => {
+    const defaultStatsObject = {
+        totalPnL: 0,
+        winRate: "0%",
+        winners: 0,
+        losers: 0,
+    }
+    return trades.reduce((prev, acc) => {
+        if (acc.outcome === Outcome.Loser) prev["losers"] += 1;
+        if (acc.outcome === Outcome.Winner) prev["winners"] += 1;
+        prev.totalPnL += acc.pnl
+        prev.winRate = `${prev.winners / (prev.winners + prev.losers) * 100}%`;
+        return prev;
+    }, { ...defaultStatsObject });
+}
+
 export const getAllTrades = (state: RootState) => state.trades;
+
+export const getTradesStats = (state: RootState) => {
+    return calcStats(state.trades)
+}
