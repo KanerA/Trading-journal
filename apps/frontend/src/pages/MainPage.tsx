a42e577394e03eafcb2a704e2e50b77eda4b55ed
kanera <kaner.assaf@gmail.com>
2025-08-28 20:54:49 +0300

    remove file extensions

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,8 +1,8 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useSelector } from 'react-redux';
-import StatsGrid from '../components/StatsGrid/StatsGrid.tsx';
-import Tabs from '../components/Tabs/Tabs.tsx';
-import { getAllTrades } from '../store/selectors/tradeSelector.ts';
+import StatsGrid from '../components/StatsGrid/StatsGrid';
+import Tabs from '../components/Tabs/Tabs';
+import { getAllTrades } from '../store/selectors/tradeSelector';
 
 export interface MainStats {
     totalPnL: number,
@@ -12,14 +12,14 @@ export interface MainStats {
 }
 
 const MainPage = () => {
-
-
     const trades = useSelector(getAllTrades)
 
-
-
+    const onClick = () => { }
     return (
         <Box>
+            <Button variant="contained" color="primary" onClick={onClick}>
+                CLICK HERE TO LOGIN
+            </Button>
             <StatsGrid />
             <Tabs trades={trades} />
         </Box>
